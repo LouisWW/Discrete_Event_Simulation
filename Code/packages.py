b2f22aeff62fb0bcca3dc4d18347fbc6f6b1171f
Louis Weyland <Louis@wcw-stud-145-109-20-55.wireless.uva.nl>
2019-11-18 14:57:15 +0100

    Add the installation of Simpy

---
+++ Code/packages.py
@@ -0,0 +1,14 @@
+##############################################################################
+
+
+# This script is used to load all the necessary packages
+
+
+
+##############################################################################
+
+# If SimPy is not install yet pleas use the following line to install 
+# it on Mac or Linux
+
+# import os
+# os.system("pip install simpy")
