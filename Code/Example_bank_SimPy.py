ef503a20cfc0b3d749c6cec8865a9b4fa77968e6
robinvandenberg73451 <robinvandenberg1998@gmail.com>
2019-11-18 22:50:03 +0100

    Changed the example of the bank teller to a carwash, because the bank teller example was in SimPy v2 instead of v3

--- Code/Example_bank_SimPy.py
+++ Code/Example_bank_SimPy.py
@@ -1,52 +1,75 @@
-## url to website with example: https://pythonhosted.org/SimPy/Tutorials/TheBank.html
-
-""" bank08: A counter with a random service time """
-from SimPy.Simulation import *
-from random import expovariate, seed
+### Got this example from: https://buildmedia.readthedocs.org/media/pdf/simpy/latest/simpy.pdf
 
 
-## Model components ------------------------
-class Source(Process):
-    """ Source generates customers randomly """
+"""
+Carwash example.
+Covers:
+- Waiting for other processes
+- Resources: Resource
+Scenario:
+A carwash has a limited number of washing machines and defines
+a washing processes that takes some (random) time.
+Car processes arrive at the carwash at a random time. If one washing
+machine is available, they start the washing process and wait for it
+to finish. If not, they wait until they an use one.
+"""
+import random
+import simpy
+RANDOM_SEED = 42
+NUM_MACHINES = 2 # Number of machines in the carwash
+WASHTIME = 5 # Minutes it takes to clean a car
+T_INTER = 7 # Create a car every ~7 minutes
+SIM_TIME = 20 # Simulation time in minutes
+class Carwash(object):
+    """A carwash has a limited number of machines (``NUM_MACHINES``) to
+    clean cars in parallel.
+    Cars have to request one of the machines. When they got one, they
+    can start the washing processes and wait for it to finish (which
+    takes ``washtime`` minutes).
+    """
+    def __init__(self, env, num_machines, washtime):
+        self.env = env
+        self.machine = simpy.Resource(env, num_machines)
+        self.washtime = washtime
+    def wash(self, car):
+        """The washing processes. It takes a ``car`` processes and tries
+        to clean it."""
+        yield self.env.timeout(WASHTIME)
+        print("Carwash removed %d%% of %s's dirt." %
+        (random.randint(50, 99), car))
 
-    def generate(self, number, meanTBA, resource):
-        for i in range(number):
-            c = Customer(name="Customer%02d" % (i,))
-            activate(c, c.visit(b=resource))
-            t = expovariate(1.0 / meanTBA)
-            yield hold, self, t
+def car(env, name, cw):
+    """The car process (each car has a ``name``) arrives at the carwash
+    (``cw``) and requests a cleaning machine.
+    It then starts the washing process, waits for it to finish and
+    leaves to never come back ...
+    """
+    print('%s arrives at the carwash at %.2f.' % (name, env.now))
+    with cw.machine.request() as request:
+        yield request
+        print('%s enters the carwash at %.2f.' % (name, env.now))
+        yield env.process(cw.wash(name))
+        print('%s leaves the carwash at %.2f.' % (name, env.now))
+def setup(env, num_machines, washtime, t_inter):
+    """Create a carwash, a number of initial cars and keep creating cars
+    approx. every ``t_inter`` minutes."""
+    # Create the carwash
+    carwash = Carwash(env, num_machines, washtime)
+    # Create 4 initial cars
+    for i in range(4):
+        env.process(car(env, 'Car %d' % i, carwash))
+    # Create more cars while the simulation is running
+    while True:
+        yield env.timeout(random.randint(t_inter - 2, t_inter + 2))
+    i += 1
+    env.process(car(env, 'Car %d' % i, carwash))
 
-
-class Customer(Process):
-    """ Customer arrives, is served and leaves """
-
-    def visit(self, b):
-        arrive = now()
-        print "%8.4f %s: Here I am     " % (now(), self.name)
-        yield request, self, b
-        wait = now() - arrive
-        print "%8.4f %s: Waited %6.3f" % (now(), self.name, wait)
-        tib = expovariate(1.0 / timeInBank)
-        yield hold, self, tib
-        yield release, self, b
-        print "%8.4f %s: Finished      " % (now(), self.name)
-
-
-## Experiment data -------------------------
-
-maxNumber = 5
-maxTime = 400.0  # minutes
-timeInBank = 12.0  # mean, minutes
-ARRint = 10.0  # mean, minutes
-theseed = 12345
-
-## Model/Experiment ------------------------------
-
-seed(theseed)
-k = Resource(name="Counter", unitName="Clerk")
-
-initialize()
-s = Source('Source')
-activate(s, s.generate(number=maxNumber, meanTBA=ARRint,
-                       resource=k), at=0.0)
-simulate(until=maxTime)
+# Setup and start the simulation
+print('Carwash')
+print('Check out http://youtu.be/fXXmeP9TvBg while simulating ... ;-)')
+random.seed(RANDOM_SEED)  # This helps reproducing the results
+# Create an environment and start the setup process
+env = simpy.Environment()
+env.process(setup(env, NUM_MACHINES, WASHTIME, T_INTER))
+# Execute!
+env.run(until=SIM_TIME)
--- Code/packages.py
+++ Code/packages.py
@@ -10,7 +10,7 @@
 # If SimPy is not install yet pleas use the following line to install 
 # it on Mac or Linux
 
-# import os
-# os.system("pip install simpy")
+import os
+os.system("pip install simpy")
 
 print("this is a tryout of git")
