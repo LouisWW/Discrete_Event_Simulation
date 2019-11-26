updated the variance everywhere and fixed the rho comparison

Code/comparedifferentrhos.py
@@ -13,17 +13,20 @@ import os
 from scipy.optimize import curve_fit
 from scipy.special import factorial
 
-n_server = 2
+n_server = 1
 mu = 0.50
 l = 0.48
-endtime = 8000
-n_simulations = 1500
+end_n_actions = 600000
+batch_size = 8000
+initialisation_period = 10000
+n_simulations = 1
+n_batches = (end_n_actions-initialisation_period)/batch_size/2.
 sjf = False  # use shortest job first
 
 '''Run simulation for different values of rho'''
 list_nf_confidence_average_queuetimes = []
 list_total_average_queuetimes = []
-mu_range = np.arange(l+0.01, 0.8, 0.03)
+mu_range = np.arange(l+0.01, 0.8, 0.05)
 for mu in mu_range:
     list_average_queuelength = []
     list_average_queuingtimes = []
@@ -38,31 +41,29 @@ for mu in mu_range:
         env = simpy.Environment()
 
         # set up the system
-        env.process(setup(env, n_server, mu, l, sjf))
+        env.process(setup(env, n_server, mu, l, sjf, end_n_actions))
 
         # run the program
-        env.run(until=endtime)
+        env.run()
 
         average_queuelength = np.average(global_variables.queue_length_list)
         list_average_queuelength.append(average_queuelength)
 
+        list_batch_averages = batch_averages(batch_size, initialisation_period)
         average_queuingtimes = np.average(global_variables.time_spend_in_queue_list)
         list_average_queuingtimes.append(average_queuingtimes)
 
         print("Now at simulation {}".format(i))
 
-    total_average_queuetimes = np.average(list_average_queuingtimes)
+    total_average_queuetimes = np.average(list_batch_averages)
     list_total_average_queuetimes.append(total_average_queuetimes)
-    variance_average_queuetimes = len(global_variables.queue_length_list) / (n_simulations - 1.) * sum((list_average_queuingtimes - total_average_queuetimes)**2)
-    nf_confidence_average_queuetimes = 1.96 * np.sqrt(variance_average_queuetimes/n_simulations)
-    list_nf_confidence_average_queuetimes.append(nf_confidence_average_queuetimes)
+    standard_deviation, confidence_interval = calc_varci(list_batch_averages, n_batches)
+    list_nf_confidence_average_queuetimes.append(confidence_interval)
 
-theoretical_waitingtime = (l/mu_range)/(l*(1.-(l/mu_range)**2))
-print(l/mu_range)
-print(l*(1.-(l/mu_range)**2))
+theoretical_waitingtime = (l/mu_range)/(l*(1.-(l/mu_range))) - 1/mu_range
 
 plt.figure()
-plt.errorbar(l/mu_range, list_total_average_queuetimes, nf_confidence_average_queuetimes)
+plt.errorbar(l/mu_range, list_total_average_queuetimes, list_nf_confidence_average_queuetimes)
 plt.plot(l/mu_range, theoretical_waitingtime, 'r')
 plt.title("Average queueing times versus rho")
 plt.xlabel("value of rho (a.u.)", fontsize=16)

Code/determinebestbatchsize.py
@@ -26,7 +26,7 @@ list_average_queuelength = []
 list_average_queuingtimes = []
 list_stddev = []
 
-diff_batchsizes = np.arange(1,20000,1000)
+diff_batchsizes = np.arange(1,30000,6000)
 
 # run the simulation multiple times
 for i in diff_batchsizes:

Code/main.py
@@ -14,10 +14,10 @@ from scipy.optimize import curve_fit
 from scipy.special import factorial
 
 n_server = 1
-mu = 0.80
-l = 0.64
-end_n_actions = 200000
-batch_size = 1000
+mu = 0.50
+l = 0.48
+end_n_actions = 600000
+batch_size = 8000
 initialisation_period = 10000
 n_simulations = 1
 n_batches = (end_n_actions-initialisation_period)/batch_size/2.
@@ -53,28 +53,28 @@ for i in range(n_simulations):
 
     print("Now at simulation {}".format(i))
 
-print(list_batch_averages, np.average(list_batch_averages))
-print("this is the sum", np.sum((list_batch_averages - np.average(list_batch_averages))**2))
-variance = 1 / (n_batches - 1) * np.sum((list_batch_averages - np.average(list_batch_averages))**2)
-print("variance", variance)
-standard_deviation = np.sqrt(variance)
+# calculate the variance
+standard_deviation, confidence_interval = calc_varci(list_batch_averages, n_batches)
+
 ########################################################################################################
 
-print("The average queueing time is {} +- {}".format(np.average(list_batch_averages), standard_deviation))
-plt.figure()
-plt.plot(list_batch_averages)
+print("The average queueing time is {} +- {}".format(np.average(list_batch_averages), confidence_interval))
 
 plt.figure()
-plt.hist(list_batch_averages)
-plt.show()
+ax = plt.gca()
+plt.xlabel("Average queuing time (a.u.)", fontsize=16, fontweight='bold')
+plt.ylabel("Occurrence (#)", fontsize=16, fontweight='bold')
+ax.xaxis.set_tick_params(labelsize=14)
+ax.yaxis.set_tick_params(labelsize=14)
+plt.hist(list_batch_averages, bins=25)
 
 
 # plot the distribution of the average queueing times
 onesim_queueingtimes = plt.figure()
 entries, bin_edges, patches = plt.hist(global_variables.time_spend_in_queue_list, bins=100, normed=True)
 plt.title("The distribution of queueing times", fontsize=14)
-plt.xlabel("Queueing times (a.u.)", fontsize=16)
-plt.ylabel("Occurrence (#)", fontsize=16)
+plt.xlabel("Queueing times (a.u.)", fontsize=16, fontweight='bold')
+plt.ylabel("Occurrence (#)", fontsize=16, fontweight='bold')
 plt.savefig("onesim_queueingtimes_distribution.png", dpi=300)
 
 ########################################################################################################
@@ -84,8 +84,8 @@ plt.savefig("onesim_queueingtimes_distribution.png", dpi=300)
 plt.figure()
 entries, bin_edges, patches = plt.hist(list_average_queuelength, bins = 100, normed=True)
 plt.title("The distribution of the average queue lengths\n of the different simulations", fontsize=14)
-plt.xlabel("Average queueing lengths (#)", fontsize=16)
-plt.ylabel("Occurrence (#)", fontsize=16)
+plt.xlabel("Average queueing lengths (#)", fontsize=16, fontweight='bold')
+plt.ylabel("Occurrence (#)", fontsize=16, fontweight='bold')
 # fit poisson distribution to data
 # x_plot = np.linspace(1, 60, 1000)
 # plt.plot(x_plot, poisson_fit(entries, bin_edges, x_plot), 'r-', lw=2)
@@ -96,8 +96,8 @@ plt.ylabel("Occurrence (#)", fontsize=16)
 # plot the distribution of servicing times
 plt.figure()
 plt.title("A histogram of the time helping costs \n of one simulation")
-plt.ylabel("Occurrence (#)", fontsize=16)
-plt.xlabel("Helping times (a.u.)", fontsize=16)
+plt.ylabel("Occurrence (#)", fontsize=16, fontweight='bold')
+plt.xlabel("Helping times (a.u.)", fontsize=16, fontweight='bold')
 plt.hist(global_variables.list_helptime)
 
 ########################################################################################################
@@ -105,8 +105,8 @@ plt.hist(global_variables.list_helptime)
 # plot the distribution of the interarrival times
 plt.figure()
 plt.title("A histogram of the time inbetween arrivals of tasks")
-plt.ylabel("Occurrence (#)", fontsize=16)
-plt.xlabel("Inter-arrival times (a.u.)", fontsize=16)
+plt.ylabel("Occurrence (#)", fontsize=16, fontweight='bold')
+plt.xlabel("Inter-arrival times (a.u.)", fontsize=16, fontweight='bold')
 plt.hist(global_variables.list_arrivaltime)
 
 ########################################################################################################
@@ -115,8 +115,8 @@ plt.hist(global_variables.list_arrivaltime)
 # plot the queue length at different times
 plt.figure()
 plt.title("Queue length versus time")
-plt.ylabel("Queue length (#)", fontsize=16)
-plt.xlabel("Time (a.u.)", fontsize=16)
+plt.ylabel("Queue length (#)", fontsize=16, fontweight='bold')
+plt.xlabel("Time (a.u.)", fontsize=16, fontweight='bold')
 plt.plot(global_variables.queue_time_list, global_variables.queue_length_list)
 
 ########################################################################################################
@@ -124,9 +124,10 @@ plt.plot(global_variables.queue_time_list, global_variables.queue_length_list)
 plt.figure()
 entries, bin_edges, patches = plt.hist(list_average_queuingtimes, bins = 100)
 plt.title("The distribution of the average queueing times\n of the different simulations", fontsize=14)
-plt.xlabel("Average queueing times (a.u.)", fontsize=16)
-plt.ylabel("Occurrence (#)", fontsize=16)
+plt.xlabel("Average queueing times (a.u.)", fontsize=16, fontweight='bold')
+plt.ylabel("Occurrence (#)", fontsize=16, fontweight='bold')
 # x_plot = np.linspace(0, 60, 1000)
 # plt.plot(x_plot, poisson_fit(entries, bin_edges, x_plot), 'r-', lw=2)
 
+plt.show()
 